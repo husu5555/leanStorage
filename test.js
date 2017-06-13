f48cac85f52952f8fabef5cd7e96a4afa688da0a
husu5555 <2279177951@qq.com>
2017-06-13 11:37:00 +0800

    test

---
+++ test.js
@@ -0,0 +1,66 @@
+var APP_ID = 'lOLo0PbqcTu3Ug1GAnJJqIT8-gzGzoHsz';
+var APP_KEY = '65f9CBuA12Pz67m0vofgCz4b';
+AV.init({
+    appId: APP_ID,
+    appKey: APP_KEY
+});
+
+// /*旅行照片*/
+// var ImgObject = AV.Object.extend('ImgObject');
+// var imgObject = new ImgObject();
+// imgObject.set("url", "http://ww3.sinaimg.cn/bmiddle/596b0666gw1ed70eavm5tg20bq06m7wi.gif");
+// imgObject.set("title", "旅行照");
+// imgObject.set("name", "Satomi_Ishihara.gif");
+// imgObject.save().then(function(object) {});
+
+// /*博客文章*/
+// var ArticleObject = AV.Object.extend('ArticleObject');
+// var articleObject = new ArticleObject();
+// articleObject.set("url", "www.baidu.com");
+// articleObject.set("title", "文章标题");
+// articleObject.set("author", "匿名");
+// articleObject.set("introduce", "文章简介");
+// articleObject.save().then(function(object) {
+
+// });
+
+/*查询图片*/
+var queryImg = new AV.Query('ImgObject');
+queryImg.exists('url');
+queryImg.find().then(function(results) {
+    $.each(results, function(index, domEle) {
+        if (index < 3) {
+            $("<img src=''/>").attr("src", domEle.get("url")).appendTo("#hobbiesImgs");
+        } else {
+            return;
+        }
+    });
+    // 换一批图片
+    $(".content__hobbies--change").on("click", function() {
+        var str;
+        for (var i = 0; i < 3; i++) {
+            var resultIndex = parseInt(Math.random() * (results.length));
+            var src = results[resultIndex].get("url");
+            $("img").eq(i).attr("src", src);
+        }
+    });
+}, function(error) {});
+
+/*查询文章*/
+var queryArticle = new AV.Query('ArticleObject');
+queryArticle.exists('title');
+queryArticle.find().then(function(results) {
+    $.each(results, function(index, domEle) {
+        if (index < 3) {
+            var title = domEle.get("title");
+            var author = domEle.get("author");
+            var introduce = domEle.get("introduce");
+            var url = domEle.get("url");
+            var str = '<div class="article-box"><div class="text-left article-box__title"><a href=' + url + '>' + title + '</a></div>';
+            str += '<div class="text-left">' + introduce + '</div></div>';
+            $(str).appendTo("#blogsArticle");
+        } else {
+            return;
+        }
+    });
+}, function(error) {});
